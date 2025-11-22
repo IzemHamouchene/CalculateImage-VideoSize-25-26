40f0ab2b0b1ba61aef7c2f41043db68b026e5f9a
IzemHamouchene <77283279+IzemHamouchene@users.noreply.github.com>
2025-11-22 22:23:14 +0100

    Update mainGrayScaleImage.c

--- config/mainGrayScaleImage.c
+++ config/mainGrayScaleImage.c
@@ -10,14 +10,14 @@ int testMethod(int input1, int input2, float output){
     char message[100];
     float result = grayScaleImage(input1, input2);
     sprintf(message, "Inputs (%d, %d). Expected output should be %f. Actuel output = %f", input1, input2, output, result);
-    assert_with_message((result >= output && result < output+1), message);
+    assert_with_message((result >= output && result < output+0.1), message);
 }
 
 int main() {
     // Test 1
-    testMethod(2, 2, 9);
+    testMethod(2, 2, 0.003);
 
     // Test 1
-    testMethod(200, 100, 19);
+    testMethod(200, 100, 19.5);
     return 0;
 }
