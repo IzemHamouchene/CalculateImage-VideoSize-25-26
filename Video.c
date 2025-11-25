05aa569d78b01938cf0495986e8df89afac94e2d
IzemHamouchene <77283279+IzemHamouchene@users.noreply.github.com>
2025-11-25 11:45:16 +0100

    Update Video.c

--- Video.c
+++ Video.c
@@ -6,7 +6,7 @@
 //   h: height of the image
 //   durationMovie: duration in second of movie (colored image)
 //   durationCredits: duration in second of credit (image Black/White)
-//   unit: Unit of the output value. It could be 'bt' byte, 'ko' kilobits, 'mo' megabits, 'go' gigabits
+//   unit: Unit of the output value. It could be 'bt' byte, 'ko' kilobytes, 'mo' megabytes, 'go' gigabytes
 // Return value
 //   video size (based on the unit passed parametter)
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
